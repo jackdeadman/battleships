5c247323c5f2953f15584fd7d4b2ee91205bef8f
Jack Deadman <jackdeadman@ymail.com>
2014-12-17 00:39:20 +0000

    made place_ship private

--- board.rb
+++ board.rb
@@ -1,6 +1,5 @@
 require './cell'
 require './gamecell'
-require 'matrix'
 require './grid'
 
 class Board
@@ -15,53 +14,6 @@ class Board
   def load_ships(ships)
     @ships = ships
     ships.each { |ship| place_ship ship }
-  end
-
-  def place_ship(ship)
-
-    can_fit = false
-
-    until can_fit
-
-      horizontal = rand(2) == 1
-
-      # Limit search space
-      if horizontal
-        max_x = @width - ship.get_size
-        max_y = @height - 1
-      else
-        max_x = @width - 1
-        max_y = @height - ship.get_size
-      end
-
-      #try the coord
-      x = rand (0..max_x)
-      y = rand (0..max_y)
-
-      can_fit = true
-
-      if horizontal
-        (0...ship.get_size).each do |index|
-          if @grid.get_cell(y, x+index).contains_ship?
-            can_fit = false
-            break
-          end
-        end
-      else
-        (0...ship.get_size).each do |index|
-          if @grid.get_cell(y+index, x).contains_ship? 
-            can_fit = false
-            break
-          end
-        end
-      end
-    end
-
-    if horizontal
-      (0...ship.get_size).each { |index| @grid.set_cell(y,x+index, GameCell.new(ship)) }
-    else
-      (0...ship.get_size).each { |index| @grid.set_cell(y+index,x, GameCell.new(ship)) }
-    end
   end
 
   def draw
@@ -107,5 +59,53 @@ class Board
   def all_destroyed?
     @ships.each { |ship| return false unless ship.destroyed?}
     true
+  end
+
+  private
+  def place_ship(ship)
+
+    can_fit = false
+
+    until can_fit
+
+      horizontal = rand(2) == 1
+
+      # Limit search space
+      if horizontal
+        max_x = @width - ship.get_size
+        max_y = @height - 1
+      else
+        max_x = @width - 1
+        max_y = @height - ship.get_size
+      end
+
+      #try the coord
+      x = rand (0..max_x)
+      y = rand (0..max_y)
+
+      can_fit = true
+
+      if horizontal
+        (0...ship.get_size).each do |index|
+          if @grid.get_cell(y, x+index).contains_ship?
+            can_fit = false
+            break
+          end
+        end
+      else
+        (0...ship.get_size).each do |index|
+          if @grid.get_cell(y+index, x).contains_ship? 
+            can_fit = false
+            break
+          end
+        end
+      end
+    end
+
+    if horizontal
+      (0...ship.get_size).each { |index| @grid.set_cell(y,x+index, GameCell.new(ship)) }
+    else
+      (0...ship.get_size).each { |index| @grid.set_cell(y+index,x, GameCell.new(ship)) }
+    end
   end
 end
